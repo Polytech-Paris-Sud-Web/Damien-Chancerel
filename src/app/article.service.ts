efde506b03535daebd524b121a1c8ad9460f9a8d
dam <damien.chancerel@gmail.com>
2022-05-19 10:15:08 +0200

    my-json-server typicode

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -11,28 +11,29 @@ export class ArticleService {
 
   constructor(private http : HttpClient) { }
 
+  private static readonly baseurl: string = "https://my-json-server.typicode.com/Polytech-Paris-Sud-Web/Damien-Chancerel";
 
   public getArticles(): Observable<Article[]> {
-    return this.http.get<Article[]>("http://localhost:3000/articles");
+    return this.http.get<Article[]>(`${ArticleService.baseurl}/articles`);
   }
 
   public getTopTen(): Observable<Article[]> {
-    return this.http.get<Article[]>("http://localhost:3000/articles?_page=1?&limit=10");
+    return this.http.get<Article[]>(`${ArticleService.baseurl}/articles?_page=1?&limit=10`);	
   }
 
   public getArticle(id: number): Observable<Article> {
-    return this.http.get<Article>(`http://localhost:3000/articles/${id}`);
+    return this.http.get<Article>(`${ArticleService.baseurl}/articles/${id}`);
   }
 
   public deleteArticle(id: number): Observable<Article> {
-    return this.http.delete<Article>(`http://localhost:3000/articles/${id}`);
+    return this.http.delete<Article>(`${ArticleService.baseurl}/articles/${id}`);
   }
 
   public addArticle(article: ArticleCreation): Observable<Article> {
-    return this.http.post<Article>("http://localhost:3000/articles", article);
+    return this.http.post<Article>(`${ArticleService.baseurl}/articles`, article);
   }
 
   public searchArticle(keyword: string): Observable<Article[]> {
-    return this.http.get<Article[]>(`http://localhost:3000/articles?q=${keyword}`);
+    return this.http.get<Article[]>(`${ArticleService.baseurl}/articles?q=${keyword}`);
   }
 }
